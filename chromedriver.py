a26acd6759557bcaa04ce7c2eb2db19d94218108
larry <larrywoo1982@gmail.com>
2016-10-30 23:03:19 +0800

    update

--- chromedriver.py
+++ chromedriver.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
+from pyvirtualdisplay import Display
 
 driver = webdriver.Chrome()
+display = Display(visible=0, size=(800, 800))  
+display.start()
 driver.get("http://www.baidu.com")
-keyword = driver.find_element_by_id("kw")
-keyword.send_keys("larrywoo")
-search_btn = driver.find_element_by_id("su")
-search_btn.click()
-xinwen = driver.find_elements_by_xpath("//*[@id='s_tab']/a[1]")
-xinwen.click()
+driver.quit()
+driver.stop()
+# keyword = driver.find_element_by_id("kw")
+# keyword.send_keys("larrywoo")
+# search_btn = driver.find_element_by_id("su")
+# search_btn.click()
+# xinwen = driver.find_elements_by_xpath("//*[@id='s_tab']/a[1]")
+# xinwen.click()

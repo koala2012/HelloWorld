60f0cb6f8b2a6336f70fd033c2c41bc4400e30ce
larry <larrywoo1982@gmail.com>
2016-10-31 15:09:19 +0800

    update by larry

--- chromedriver.py
+++ chromedriver.py
@@ -1,16 +1,13 @@
-from selenium import webdriver
-from pyvirtualdisplay import Display
 
-# add following two lines code before start the webdriver
-display = Display(visible=0, size=(800, 800))  
-display.start()
-driver = webdriver.Chrome()
-driver.get("http://www.baidu.com")
+import time
+from selenium import webdriver
+
+#driver = webdriver.Chrome('/usr/bin/chromedriver')  # Optional argument, if not specified will search path.
+driver = webdriver.Firefox()
+driver.get('http://www.google.com/xhtml');
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
 driver.quit()
-driver.stop()
-# keyword = driver.find_element_by_id("kw")
-# keyword.send_keys("larrywoo")
-# search_btn = driver.find_element_by_id("su")
-# search_btn.click()
-# xinwen = driver.find_elements_by_xpath("//*[@id='s_tab']/a[1]")
-# xinwen.click()

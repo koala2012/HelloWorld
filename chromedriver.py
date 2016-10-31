5482faf81fcedba8ff7165ce22fc9b8af001eeda
larry <larrywoo1982@gmail.com>
2016-10-31 22:52:35 +0800

    update by larry

--- chromedriver.py
+++ chromedriver.py
@@ -8,8 +8,8 @@ from xvfbwrapper import Xvfb
 # xf = Xvfb()  #  xf = Xvfb(1920, 1080) - will create virtual display with 1920x1080 size
 # xf.start()
 # display.start()
-# driver = webdriver.Chrome('/usr/bin/chromedriver')  # Optional argument, if not specified will search path.
-driver = webdriver.Firefox()
+driver = webdriver.Chrome('/usr/bin/chromedriver')  # Optional argument, if not specified will search path.
+# driver = webdriver.Firefox()
 driver.get('http://www.baidu.com');
 time.sleep(5) # Let the user actually see something!
 search_box = driver.find_element_by_name('wd')

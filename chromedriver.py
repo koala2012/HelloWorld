02bc80a6eb76c4f42d1aba52aa0a2a7fba76eb8f
larry <larrywoo1982@gmail.com>
2016-10-31 22:23:30 +0800

    update by larry

--- chromedriver.py
+++ chromedriver.py
@@ -3,11 +3,11 @@ import time
 from selenium import webdriver
 from pyvirtualdisplay import Display
 
-display = Display(visible=0, size=(800, 600))
-display.start()
-driver = webdriver.Chrome('/usr/bin/chromedriver')  # Optional argument, if not specified will search path.
-# driver = webdriver.Firefox()
-driver.get('http://www.google.com/xhtml');
+# display = Display(visible=0, size=(800, 600))
+# display.start()
+# driver = webdriver.Chrome('/usr/bin/chromedriver')  # Optional argument, if not specified will search path.
+driver = webdriver.Firefox()
+driver.get('http://www.baidu.com');
 time.sleep(5) # Let the user actually see something!
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('ChromeDriver')
